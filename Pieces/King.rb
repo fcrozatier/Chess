e8a1cf1213d74c003ba421f395356985d483cf97
Frederic Crozatier <frederic.crozatier@protonmail.com>
2020-01-02 17:10:39 +0100

    implement Queen and King #possible_moves

--- Pieces/King.rb
+++ Pieces/King.rb
@@ -4,4 +4,14 @@ class King < Piece
     super(color, position)
   end
 
+  def possible_moves
+    board = Board.instance
+    moves = []
+    ["left", "right", "up", "down","upleft", "upright", "downleft", "downright"].each do |prefix|
+      moves += board.method(prefix).call(position).first(1)
+        .select { |cell| cell.empty? || cell.piece.color != color }
+    end
+    moves
+  end
+
 end
